7d0bd934ff5ca8e1dae855fe9acc47ececffb33d
Adelaide <adelaidependo703@gmail.com>
2026-09-21 13:38:15 +0300

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ 
+ int main() {
+ 	//print a header line
+ 	printf("Units offered this semester:\n");
+ 	
+ 	printf("Introduction to Programming\n");
+ 	printf("Architecture\n");
+ 	printf("Computer Networks\n");
+ 	printf("Database Systems\n");
+ 	printf("Software Engineering\n");
+ 	
+ 	return 0; 
+ }
